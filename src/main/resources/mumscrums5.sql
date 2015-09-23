5ccbeeb5a2ed98cadfe98d359532f49c754ee70b
Sudeep Bagale <bgsudeep@BG-MBP.local>
2015-09-22 22:28:51 -0500

    latest sql added

--- src/main/resources/mumscrums5.sql
+++ src/main/resources/mumscrums5.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 18, 2015 at 01:44 AM
+-- Generation Time: Sep 23, 2015 at 03:27 AM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -58,15 +58,16 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `telephone` varchar(255) DEFAULT NULL,
   `zip` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `employee`
 --
 
 INSERT INTO `employee` (`id`, `city`, `email`, `firstName`, `joinDate`, `lastName`, `salary`, `state`, `street`, `telephone`, `zip`) VALUES
-(1, 'Fairfield', 'bgsudeep@gmail.com', 'Sudeep', '16-09-2015', 'Bagale', '100000', 'Iowa', '1000 North 4th Street', '6462807357', '52557'),
-(2, 'Fairfield', 'bgsudeep@gmail.com', 'Sudeep', '16-09-2015', 'Bagale', '123', 'Iowa', '1000 North 4th Street', '6462807357', '52557');
+(1, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'),
+(3, 'Fairfield', 'bgsudeep@gmail.com', 'Sudeep', '09-09-2015', 'Bagale', '1000000', 'IA', '1000 North 4th Street', '646-280-7357', '52557'),
+(4, 'Fairfield', 'saugatligal@gmail.com', 'Saugat', '09-09-2015', 'Ligal', '1000000', 'IA', '1000 North 4th Street', '262-271-4867', '52557');
 
 -- --------------------------------------------------------
 
@@ -88,7 +89,8 @@ CREATE TABLE IF NOT EXISTS `employee_user` (
 
 INSERT INTO `employee_user` (`user_id`, `employee_id`) VALUES
 (1, 1),
-(2, 2);
+(3, 3),
+(4, 4);
 
 -- --------------------------------------------------------
 
@@ -108,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `product_backlog` (
 --
 
 INSERT INTO `product_backlog` (`id`, `description`, `title`) VALUES
-(1, 'This is a description to create a watch', 'Create Watch');
+(1, 'PB 1 Desc', 'PB 1');
 
 -- --------------------------------------------------------
 
@@ -122,15 +124,19 @@ CREATE TABLE IF NOT EXISTS `release_backlog` (
   `endDate` datetime DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `startDate` datetime DEFAULT NULL,
+  `assigned_to` int(11) DEFAULT NULL,
   `product_backlog_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKE715F8C5E3FCEAD2` (`product_backlog_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `FKE715F8C5E3FCEAD2` (`product_backlog_id`),
+  KEY `FKE715F8C5B987D751` (`assigned_to`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `release_backlog`
 --
 
+INSERT INTO `release_backlog` (`id`, `description`, `endDate`, `name`, `startDate`, `assigned_to`, `product_backlog_id`) VALUES
+(1, 'RB 1 Desc', '2015-12-12 00:00:00', 'RB 1', '2015-09-11 00:00:00', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -142,17 +148,18 @@ CREATE TABLE IF NOT EXISTS `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`id`, `role`) VALUES
-(1, 'Product Owner'),
-(2, 'Scrum Master'),
-(3, 'Developer'),
-(4, 'Tester');
+(5, 'Scrum Master'),
+(2, 'Developer'),
+(3, 'Tester'),
+(4, 'Product Owner'),
+(1, 'HR ADMIN');
 
 -- --------------------------------------------------------
 
@@ -167,12 +174,14 @@ CREATE TABLE IF NOT EXISTS `sprint` (
   `release_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FKCA9D021A734DFB63` (`release_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `sprint`
 --
 
+INSERT INTO `sprint` (`id`, `description`, `title`, `release_id`) VALUES
+(1, 'Sprint 1 Desc', 'Sprint 1', 1);
 
 -- --------------------------------------------------------
 
@@ -186,15 +195,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `enabled`, `password`, `username`) VALUES
-(1, '', 'hello', 'bgsudeep'),
-(2, '', 'admin', 'admin');
+(1, '', 'admin', 'admin'),
+(3, '', 'hello', 'bgsudeep'),
+(4, '', 'hello', 'saugatligal');
 
 -- --------------------------------------------------------
 
@@ -214,8 +224,9 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 --
 
 INSERT INTO `user_role` (`role_id`, `user_id`) VALUES
-(2, 1),
-(1, 2);
+(1, 1),
+(4, 3),
+(3, 4);
 
 -- --------------------------------------------------------
 
@@ -237,13 +248,20 @@ CREATE TABLE IF NOT EXISTS `user_story` (
   `product_backlog_id` int(11) NOT NULL,
   `release_backlog_id` int(11) DEFAULT NULL,
   `sprint_id` int(11) DEFAULT NULL,
+  `assigned_developer` int(11) DEFAULT NULL,
+  `assigned_tester` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK73530381839D89E5` (`release_backlog_id`),
   KEY `FK73530381E3FCEAD2` (`product_backlog_id`),
-  KEY `FK73530381E24FA351` (`sprint_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `FK73530381E24FA351` (`sprint_id`),
+  KEY `FK735303816A35735E` (`assigned_developer`),
+  KEY `FK7353038180E0CD15` (`assigned_tester`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user_story`
 --
 
+INSERT INTO `user_story` (`id`, `assignedDate`, `completedDate`, `createdDate`, `description`, `estimatedDevHours`, `estimatedTestHours`, `remainingDevHours`, `remainingTestHours`, `title`, `product_backlog_id`, `release_backlog_id`, `sprint_id`, `assigned_developer`, `assigned_tester`) VALUES
+(1, NULL, NULL, NULL, 'US 1 Desc', 12, 12, 12, 12, 'US 1', 1, 1, 1, NULL, NULL),
+(2, NULL, NULL, NULL, 'US 2 Desc', 23, 23, 23, 23, 'US2', 1, 1, 1, NULL, 4);
